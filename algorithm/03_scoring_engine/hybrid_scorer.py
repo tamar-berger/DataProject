ae57e9aa8934efb96abb6b6776b0985984396ee0
Adi Zuarets <adizuarets@gmail.com>
2025-09-21 21:04:20 +0300

    Update hybrid_scorer.py

--- algorithm/03_scoring_engine/hybrid_scorer.py
+++ algorithm/03_scoring_engine/hybrid_scorer.py
@@ -27,8 +27,8 @@ class HybridScorer:
     """
     
     def __init__(self, 
-                 content_weight: float = 0.5,
-                 similarity_weight: float = 0.5,
+                 content_weight: float = 0.6,
+                 similarity_weight: float = 0.4,
                  min_ratings_for_similarity: int = 1,
                  confidence_threshold: float = 0.3):
         """
